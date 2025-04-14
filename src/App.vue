b87a49186d04a71f46095439db8800b49f0bb753
Salauddin Sakib <salauddinsakib99@gmail.com>
2025-04-14 10:27:05 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <HelloWorld msg="vite + vue"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1 class="text-red-500 text-6xl font-bold text-center bg-amber-300 h-screen flex items-center justify-center">Alhamdulillah</h1>
+</template>
+
